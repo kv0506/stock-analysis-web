19a72ea65449a03c13c88826bf33fc8f99f1c49a
Kumaran Vellaisamy <kumaranvellaisamy@outlook.com>
2023-03-10 01:23:21 +0100

    ticker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {UsersComponent} from './users/users.component';
 import {RouterModule} from "@angular/router";
+import {WidgetsModule} from "./shared/widgets/widgets.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    UsersComponent
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
+    WidgetsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
       {path: 'users', component: UsersComponent},
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Index, IndexCollection} from "../shared/models";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public indexes: Array<Index>;
 
+  constructor(private http: HttpClient) {
+  }
+
+  public ngOnInit() {
+    this.getIndexes();
+  }
+
+  private getIndexes() {
+    this.http.get<IndexCollection>('https://stock-analysis.azurewebsites.net/api/live-market').subscribe((resp: IndexCollection) => {
+      this.indexes = resp.result;
+    })
+  }
+
+  private getUsers(){
+    
+  }
 }
